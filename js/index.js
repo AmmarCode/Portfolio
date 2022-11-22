9499528829d5a5d1d9b0d90474fa835682d706bd
AmmarCode <ammar.code01@gmail.com>
2022-11-22 03:24:51 -0500

    Update look

--- js/index.js
+++ js/index.js
@@ -1,78 +1,57 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav-link')
+// const navToggle = document.querySelector(".nav-toggle");
+// const navLinks = document.querySelectorAll(".nav-link");
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
+// navToggle.addEventListener("click", () => {
+//   document.body.classList.toggle("nav-open");
+// });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    });
-})
+// navLinks.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     document.body.classList.remove("nav-open");
+//   });
+// });
 
+// const TypeWriter = function (txtElement, words, wait = 1000) {
+//   this.txtElement = txtElement;
+//   this.words = words;
+//   this.txt = "";
+//   this.wordIndex = 0;
+//   this.wait = parseInt(wait, 12);
+//   this.type();
+// };
 
-const TypeWriter = function (txtElement, words, wait = 1000) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = '';
-    this.wordIndex = 0;
-    this.wait = parseInt(wait, 12);
-    this.type();
-    this.isDeleting = false;
-}
+// // Type Method
+// TypeWriter.prototype.type = function () {
+//   // Current index of word
+//   const current = this.wordIndex % this.words.length;
+//   // Get full text of the current word
+//   const fullTxt = this.words[current];
 
-// Type Method
-TypeWriter.prototype.type = function () {
-    // Current index of word
-    const current = this.wordIndex % this.words.length;
-    // Get full text of the current word
-    const fullTxt = this.words[current]
+//   // Check if deleting
+//   if (this.isDeleting) {
+//     // Remove char
+//     this.txt = fullTxt.substring(0, this.txt.length - 1);
+//   } else {
+//     // Add char
+//     this.txt = fullTxt.substring(0, this.txt.length + 1);
+//   }
 
-    // Check if deleting
-    if (this.isDeleting) {
-        // Remove char
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        // Add char
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+//   // Insert txt into element
+//   this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-    // Insert txt into element
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+//   // Initial Type speed
+//   let typeSpeed = 50;
 
-    // Initial Type speed
-    let typeSpeed = 60;
+//   setTimeout(() => this.type(), typeSpeed);
+// };
 
-    if (this.isDeleting) {
-        typeSpeed /= 2;
-    }
-
-    // If word is complete
-    if (!this.isDeleting && this.txt === fullTxt) {
-        // Make pause at end
-        typeSpeed = this.wait;
-        // Set delete to true
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        // Move to next word
-        this.wordIndex++;
-        // Pause before start typing
-        typeSpeed = 500;
-    }
-
-    setTimeout(() => this.type(), typeSpeed)
-}
-
-
-// Init on DOM load
-document.addEventListener('DOMContentLoaded', init);
-// Init App
-function init() {
-    const txtElement = document.querySelector('.txt-type');
-    const words = JSON.parse(txtElement.getAttribute('data-words'));
-    const wait = txtElement.getAttribute('data-wait');
-    // Init TypeWriter
-    new TypeWriter(txtElement, words, wait);
-}
+// // Init on DOM load
+// document.addEventListener("DOMContentLoaded", init);
+// // Init App
+// function init() {
+//   const txtElement = document.querySelector(".txt-type");
+//   const words = JSON.parse(txtElement.getAttribute("data-words"));
+//   const wait = txtElement.getAttribute("data-wait");
+//   // Init TypeWriter
+//   new TypeWriter(txtElement, words, wait);
+// }
